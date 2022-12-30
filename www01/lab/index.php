7f782785a9abb612e189ea9aea47178bf316d9c2
Peter Fletcher <peter@milneandfletch.co.uk>
2022-12-30 16:11:25 +0000

    update

---
+++ www01/lab/index.php
@@ -0,0 +1,178 @@
+<table style="width: 800px; border: 1px solid black">
+  <tr>
+    <td width=200px>pfsense.home.lab</td>
+    <td>
+      <a href="https://pedro-pfsense-01.home.lab/" target="_blank"><img src="./images/pfsense-logo.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-SWCH-01.home.lab</td>
+    <td>
+      <a href="http://Pedro-SWCH-01.home.lab" target="_blank"><img src="./images/netgear.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>WebServers Service (LB)</td>
+    <td>
+      <a href="https://webservers.pedro.home/" target="_blank"><img src="./images/webservers.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank">  </a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>Pedro-pfsense-02.home.lab</td>
+    <td>
+      <a href="https://pedro-pfsense-02.home.lab/" target="_blank"><img src="./images/pfsense-logo.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-BIG-01.home.lab</td>
+    <td>
+      <a href="https://Pedro-BIGIP-01.home.lab/tmui/" target="_blank"><img src="./images/f5.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-Web-01</td>
+    <td>
+      <a href="http://pedro-web-01.home.lab" target="_blank"><img src="./images/webserver.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>Pedro-pfsense-03.home.lab</td>
+    <td>
+      <a href="https://pedro-pfsense-03.home.lab/" target="_blank"><img src="./images/pfsense-logo.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-BIG-02.home.lab</td>
+    <td>
+      <a href="https://Pedro-BIGIP-02.home.lab/tmui/" target="_blank"><img src="./images/f5.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-Web-02</td>
+    <td>
+      <a href="http://pedro-web-02.home.lab" target="_blank"><img src="./images/webserver.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>Pedro-opnsense-01.home.lab</td>
+    <td>
+      <a href="https://192.168.201.82/" target="_blank"><img src="./images/opnsense.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-MCRV-01.home.lab</td>
+    <td>
+    <a href="https://192.168.201.81:4712/Konfigurator/html/" target="_blank"><img src="./images/mcafee-web-gateway.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Pedro-Web-03</td>
+    <td>
+      <a href="http://pedro-web-03.home.lab" target="_blank"><img src="./images/webserver.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a> 
+    </td>
+  </tr>
+
+  <tr>
+    <td>Pedro-host-01.home.lab</td>
+    <td>
+      <a href="https://pedro-host-01.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>Guacamole.home.lab</td>
+    <td>
+    <a href="http://dockerdmz.home.lab:8080/guacamole/" target="_blank"><img src="./images/guacamole.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-host-01.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td> 
+  </tr>
+
+  <tr>
+    <td>Pedro-host-02.home.lab</td>
+    <td>
+      <a href="https://pedro-host-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>ipam.home.lab</td>
+    <td>
+      <a href="http://pedro-docker-01.home.lab:3080/" target="_blank"><img src="./images/phpipam_icon.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a> 
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-host-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>Pedro-esxi-01.home.lab</td>
+    <td>
+      <a href="https://pedro-esxi-01.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-esxi-01.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-esxi-01.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>Pedro-esxi-02.home.lab</td>
+    <td>
+      <a href="https://pedro-esxi-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-esxi-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-esxi-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+  </tr>
+
+  <tr>
+    <td>Truenas.home.lab</td>
+    <td>
+      <a href="https://truenas.home.lab" target="_blank"><img src="./images/trueNAS.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-esxi-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+    <td>XXXX</td>
+    <td>
+      <a href="https://pedro-esxi-02.home.lab/ui/" target="_blank"><img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank"></a>
+    </td>
+  </tr>
+</table>
+
+
+
+<table style="width:400px">
+  <tr>
+    <td>border.warwick.lab</td>
+    <td>corpvpn.warwick.lab</td>
+    <td>Kibana - Warwick</td>
+    <td>Esxi - Warwick</td>
+  </tr>
+
+  <tr>
+    <td>
+      <a href="https://192.168.99.30" target="_blank">
+        <img src="./images/pfsense-logo.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank">
+      </a>
+    </td>
+    <td>
+      <a href="https://192.168.11.1" target="_blank">
+        <img src="./images/pfsense-logo.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank">
+      </a>
+    </td>
+    <td>
+      <a href="http://data01.warwick.lab" target="_blank">
+        <img src="./images/kibana.png" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank">  
+      </a>  
+    </td>
+    <td>
+      <a href="https://137.221.129.168" target="_blank">
+        <img src="./images/esxi.jpg" alt="Girl in a jacketl" style="width:75px;height:75px;" target="_blank">
+      </a>
+    </td>
+  </tr>
+
+</table>
+
+
+
+
+ 
+
+
