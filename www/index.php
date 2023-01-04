faae1807433610d0f710048935f921e4f028932f
Peter Fletcher <peter@milneandfletch.co.uk>
2023-01-04 14:15:08 +0000

    update

--- www/index.php
+++ www/index.php
@@ -98,6 +98,22 @@ foreach ($_SERVER as $parm => $value)  echo "$parm = '$value'\n\r";
 
 <?php
 
+if (!function_exists('each')) {
+    function each(array &$array) {
+        $value = current($array);
+        $key = key($array);
+
+        if (is_null($key)) {
+            return false;
+        }
+
+        // Move pointer.
+        next($array);
+
+        return array(1 => $value, 'value' => $value, 0 => $key, 'key' => $key);
+    }
+}
+
 $gethost=gethostname();
 
 echo '<table style="width:100%">';
